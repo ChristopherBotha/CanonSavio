fb308fae86005a41838606699da5629d5f7629e6
ChristopherBotha <christopher@webhounds.co.za>
2022-11-15 18:06:19 +0200

    standards updated

---
+++ scripts/Game.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+var playerDamage : float 
+var playerMaxMana : float
+var playerMana : float
+var playerHealth : float 
+var playerExp : float
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ scripts/SignalBus.gd
@@ -0,0 +1,22 @@
+#player changes
+signal updatePlayerHealth(val)
+signal updatePlayerMana(val)
+
+#player upgrades / stats
+signal updatePlayerMaxHealth(val)
+signal updatePlayerDamage(val)
+signal updatePlayerMaxMana(val)
+
+#Game Machanics
+signal updateExperience(val)
+signal updateGold(val)
+signal updateScore(val)
+
+#world interaction
+signal interact(val)
+
+#Enemies
+signal updateEnemiesDamage(val)
+signal enemyCounter(val)
+
+
