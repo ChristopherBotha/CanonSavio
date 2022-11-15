1d5029380c40ce96e13c8437187753bfc64558d8
ChristopherBotha <christopher@webhounds.co.za>
2022-11-15 21:29:46 +0200

    friction

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,11 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var maxSpeed : float = 10.0
+var minSpeed : float = 0.0
+var maxJumpHeight = 10.0
+
+var friction : float = 0.05
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,18 +19,23 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
+		while velocity.y < maxJumpHeight:
+			velocity.y += JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+	elif is_on_floor() and direction == Vector3.ZERO:
+		velocity = velocity.lerp(Vector3.ZERO, friction ) # move_toward(velocity.x, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 	move_and_slide()
