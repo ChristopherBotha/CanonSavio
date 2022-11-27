528ed6655543c2064e59d4f7881d4f32c3c0d01c
ChristopherBotha <christopher@webhounds.co.za>
2022-11-27 23:48:58 +0200

    updated

--- scripts/timeZone.gd
+++ scripts/timeZone.gd
@@ -1,18 +1,26 @@
 extends Area3D
 
+var zoned : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-#	$MeshInstance3D.visible = false
-#	$Area3D/CollisionShape3D.disabled = true
-	pass	
+	$MeshInstance3D.visible = false
+	$CollisionShape3D.disabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("timeZone"):
+	
+	if Input.is_action_just_pressed("timeZone") and zoned == false:
+		zoned = true
 		$AnimationPlayer.play("grow")
-#		$MeshInstance3D.visible = true
-#		$Area3D/CollisionShape3D.disabled = false
-	pass
+		$MeshInstance3D.visible = true
+		$CollisionShape3D.disabled = false
+		
+		await get_tree().create_timer(1).timeout
+		zoned = false
+		$AnimationPlayer.play("RESET")
+		$MeshInstance3D.visible = false
+		$CollisionShape3D.disabled = true
 
 func _on_area_entered(area: Area3D) -> void:
 	if area.is_in_group("Bullets"):
